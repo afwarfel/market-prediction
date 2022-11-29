53328a464c6850e0d9c8fe3b1d51d770f5f41411
alexwarfel@gmail.com <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>
2022-11-28 21:59:20 -0800

    Update for solicit inferences

--- machine_learning/solicit_inferences.py
+++ machine_learning/solicit_inferences.py
@@ -6,8 +6,41 @@ from generate_training_data import capture_fred_series
 import ta
 import pandas as pd
 import numpy as np
+import boto3
+import botocore
+from io import StringIO
 
-def solicit_inferences(fred_api_key):
+def check_s3_bucket_for_previous_predictions(aws_access_key_id, aws_secret_access_key, bucket_name):
+
+    s3 = boto3.resource(service_name='s3',aws_access_key_id=aws_access_key_id,
+                        aws_secret_access_key=aws_secret_access_key)
+
+    inferences = pd.DataFrame(columns=['date','prediction'])
+
+    try:
+        inferences = s3.Object(bucket_name, 'inferences.csv').load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print('No previous predictions found in S3 bucket.')
+        else:
+            raise ValueError('Error accessing S3 bucket.')
+    else:
+        print('Previous predictions found in S3 bucket. Downloading...')
+        inferences = pd.read_csv(s3.Object(bucket_name, 'inferences.csv').get()['Body'])
+        
+    return inferences
+
+def upload_inferences_to_s3(aws_access_key_id, aws_secret_access_key, bucket_name, inferences):
+
+    s3 = boto3.resource(service_name='s3',aws_access_key_id=aws_access_key_id,
+                        aws_secret_access_key=aws_secret_access_key)
+
+    csv_buffer = StringIO()
+    inferences.to_csv(csv_buffer, index=False)
+    s3.Object(bucket_name, 'inferences.csv').put(ACL='public-read',Body=csv_buffer.getvalue())
+
+def solicit_inferences(fred_api_key, aws_access_key_id, aws_secret_access_key, bucket_name):
+
 
     trained_columns = ['dff', 'bamlh0a0hym2', 'dcoilwtico', 'vixcls', 't10y3m', 'month', 'weekday', 'quarter', 'week', 'dff_aroon_indicator', 'dff_ema_indicator', 'dff_macd_diff', 'dff_trix', 'dff_sma_26', 'dff_sma_12', 'dff_sma_9', 'dff_diff', 'bamlh0a0hym2_aroon_indicator', 'bamlh0a0hym2_ema_indicator', 'bamlh0a0hym2_macd_diff', 'bamlh0a0hym2_trix', 'bamlh0a0hym2_sma_26', 'bamlh0a0hym2_sma_12', 'bamlh0a0hym2_sma_9', 'bamlh0a0hym2_diff', 'dcoilwtico_aroon_indicator', 'dcoilwtico_ema_indicator', 'dcoilwtico_macd_diff', 'dcoilwtico_trix', 'dcoilwtico_sma_26', 'dcoilwtico_sma_12', 'dcoilwtico_sma_9', 'dcoilwtico_diff', 'vixcls_aroon_indicator', 'vixcls_ema_indicator', 'vixcls_macd_diff', 'vixcls_trix', 'vixcls_sma_26', 'vixcls_sma_12', 'vixcls_sma_9', 'vixcls_diff', 'nasdaqcom_aroon_indicator', 'nasdaqcom_ema_indicator', 'nasdaqcom_macd_diff',
                     'nasdaqcom_trix', 'nasdaqcom_sma_26', 'nasdaqcom_sma_12', 'nasdaqcom_sma_9', 'nasdaqcom_log', 't10y3m_aroon_indicator', 't10y3m_ema_indicator', 't10y3m_macd_diff', 't10y3m_trix', 't10y3m_sma_26', 't10y3m_sma_12', 't10y3m_sma_9', 't10y3m_diff', 'month_aroon_indicator', 'month_ema_indicator', 'month_macd_diff', 'month_trix', 'month_sma_26', 'month_sma_12', 'month_sma_9', 'month_diff', 'weekday_aroon_indicator', 'weekday_ema_indicator', 'weekday_macd_diff', 'weekday_trix', 'weekday_sma_26', 'weekday_sma_12', 'weekday_sma_9', 'weekday_diff', 'quarter_aroon_indicator', 'quarter_ema_indicator', 'quarter_macd_diff', 'quarter_trix', 'quarter_sma_26', 'quarter_sma_12', 'quarter_sma_9', 'quarter_diff', 'week_aroon_indicator', 'week_ema_indicator', 'week_macd_diff', 'week_trix', 'week_sma_26', 'week_sma_12', 'week_sma_9', 'week_diff']
@@ -66,28 +99,35 @@ def solicit_inferences(fred_api_key):
 
     prediction = model.predict(most_recent_records)
 
-    if path.exists(os.path.join('data', 'prediction.csv')):
-        prediction_df = pd.read_csv(os.path.join('data', 'prediction.csv'))
-        prediction_df['date'] = pd.to_datetime(prediction_df['date'])
+    print('Checking for previous predictions in S3 bucket...')
+    prediction_df = check_s3_bucket_for_previous_predictions(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, bucket_name=bucket_name)
 
-        if most_recent_records.index[0] in prediction_df['date'].values:
-            print('Prediction already exists')
-        else:
-            prediction_df = prediction_df.append(
-                {'date': most_recent_records.index[0], 'prediction': prediction[0]}, ignore_index=True)
+    prediction_df['date'] = pd.to_datetime(prediction_df['date'])
+    most_recent_records.index = pd.to_datetime(most_recent_records.index)
+
+    if most_recent_records.index[0] in prediction_df['date'].values:
+        print('Most recent prediction already exists in S3 bucket.')
+        pass
     else:
-        prediction_df = pd.DataFrame(
-            {'date': most_recent_records.index, 'prediction': prediction})
+        print('Most recent prediction does not exist in S3 bucket. Adding...')
+        prediction_df = pd.concat([prediction_df, pd.DataFrame({'date': most_recent_records.index[0], 'prediction': prediction[0]},index=[0])], axis=0, ignore_index=True)
 
-    try:
-        prediction_df.to_csv(os.path.join('data', 'prediction.csv'), index=False)
-    except:
-        prediction_df.to_csv(os.path.join('machine_learning', 'data', 'prediction.csv'), index=False)
+    print('Saving predictions to S3 bucket...')
+    upload_inferences_to_s3(
+        inferences=prediction_df, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, bucket_name=bucket_name)
 
     print(prediction_df)
 
-    return prediction
+    return
 
-# Note that sys.argv[0] is the script name itself and can be ignored
-fred_api_key = str(sys.argv[1])
-solicit_inferences(fred_api_key = fred_api_key)
+if __name__ == '__main__':
+    # Note that sys.argv[0] is the script name itself and can be ignored
+    fred_api_key = str(sys.argv[1])
+    aws_access_key_id = str(sys.argv[2])
+    aws_secret_access_key = str(sys.argv[3])
+    aws_bucket_name = str(sys.argv[4])
+
+    solicit_inferences(fred_api_key=fred_api_key, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, bucket_name=aws_bucket_name)
+    # i = check_s3_bucket_for_previous_predictions(aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key, bucket_name=aws_bucket_name)
+    # print(i)
