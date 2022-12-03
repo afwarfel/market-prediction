614c1ffb96862da5d0b1b0ccd50b29e7faeddad2
alexwarfel@gmail.com <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>
2022-12-03 10:16:54 -0800

    Update for solicit inferences

--- machine_learning/solicit_inferences.py
+++ machine_learning/solicit_inferences.py
@@ -11,7 +11,7 @@ import botocore
 from io import StringIO
 import json
 
-def check_s3_bucket_for_previous_predictions(aws_access_key_id, aws_secret_access_key, bucket_name):
+def check_s3_bucket_for_previous_predictions(aws_access_key_id, aws_secret_access_key, bucket_name, file_name):
 
     s3 = boto3.resource(service_name='s3',aws_access_key_id=aws_access_key_id,
                         aws_secret_access_key=aws_secret_access_key)
@@ -19,38 +19,41 @@ def check_s3_bucket_for_previous_predictions(aws_access_key_id, aws_secret_acces
     inferences = pd.DataFrame(columns=['date','prediction'])
 
     try:
-        inferences = pd.read_json(s3.Object(bucket_name, 'data/inferences.json').get()['Body'])
+        inferences = pd.read_json(s3.Object(bucket_name, 'data/'+file_name+'.json').get()['Body'])
     except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print('No previous predictions found in S3 bucket.')
-        else:
-            raise ValueError('Error accessing S3 bucket.')
+        print('Error:',e)
     else:
         print('Previous predictions found in S3 bucket. Downloading...')
-        inferences = pd.read_json(s3.Object(bucket_name, 'data/inferences.json').get()['Body'])
+        inferences = pd.read_json(s3.Object(bucket_name, 'data/'+file_name+'.json').get()['Body'])
         
     return inferences
 
-def upload_inferences_to_s3(aws_access_key_id, aws_secret_access_key, bucket_name, inferences):
+def upload_inferences_to_s3(aws_access_key_id, aws_secret_access_key, bucket_name, inferences, file_name):
 
     s3 = boto3.resource(service_name='s3',aws_access_key_id=aws_access_key_id,
                         aws_secret_access_key=aws_secret_access_key)
 
     csv_buffer = StringIO()
     inferences.to_json(csv_buffer)
-    s3.Object(bucket_name, 'data/inferences.json').put(ACL='public-read',Body=csv_buffer.getvalue())
+    s3.Object(bucket_name, 'data/'+file_name+'.json').put(ACL='public-read',Body=csv_buffer.getvalue())
 
 def solicit_inferences(fred_api_key, aws_access_key_id, aws_secret_access_key, bucket_name):
 
 
-    trained_columns = ['dff', 'bamlh0a0hym2', 'dcoilwtico', 'vixcls', 't10y3m', 'month', 'weekday', 'quarter', 'week', 'dff_aroon_indicator', 'dff_ema_indicator', 'dff_macd_diff', 'dff_trix', 'dff_sma_26', 'dff_sma_12', 'dff_sma_9', 'dff_diff', 'bamlh0a0hym2_aroon_indicator', 'bamlh0a0hym2_ema_indicator', 'bamlh0a0hym2_macd_diff', 'bamlh0a0hym2_trix', 'bamlh0a0hym2_sma_26', 'bamlh0a0hym2_sma_12', 'bamlh0a0hym2_sma_9', 'bamlh0a0hym2_diff', 'dcoilwtico_aroon_indicator', 'dcoilwtico_ema_indicator', 'dcoilwtico_macd_diff', 'dcoilwtico_trix', 'dcoilwtico_sma_26', 'dcoilwtico_sma_12', 'dcoilwtico_sma_9', 'dcoilwtico_diff', 'vixcls_aroon_indicator', 'vixcls_ema_indicator', 'vixcls_macd_diff', 'vixcls_trix', 'vixcls_sma_26', 'vixcls_sma_12', 'vixcls_sma_9', 'vixcls_diff', 'nasdaqcom_aroon_indicator', 'nasdaqcom_ema_indicator', 'nasdaqcom_macd_diff',
+    trained_columns_classifier = ['dff', 'bamlh0a0hym2', 'dcoilwtico', 'vixcls', 't10y3m', 'month', 'weekday', 'quarter', 'week', 'dff_aroon_indicator', 'dff_ema_indicator', 'dff_macd_diff', 'dff_trix', 'dff_sma_26', 'dff_sma_12', 'dff_sma_9', 'dff_diff', 'bamlh0a0hym2_aroon_indicator', 'bamlh0a0hym2_ema_indicator', 'bamlh0a0hym2_macd_diff', 'bamlh0a0hym2_trix', 'bamlh0a0hym2_sma_26', 'bamlh0a0hym2_sma_12', 'bamlh0a0hym2_sma_9', 'bamlh0a0hym2_diff', 'dcoilwtico_aroon_indicator', 'dcoilwtico_ema_indicator', 'dcoilwtico_macd_diff', 'dcoilwtico_trix', 'dcoilwtico_sma_26', 'dcoilwtico_sma_12', 'dcoilwtico_sma_9', 'dcoilwtico_diff', 'vixcls_aroon_indicator', 'vixcls_ema_indicator', 'vixcls_macd_diff', 'vixcls_trix', 'vixcls_sma_26', 'vixcls_sma_12', 'vixcls_sma_9', 'vixcls_diff', 'nasdaqcom_aroon_indicator', 'nasdaqcom_ema_indicator', 'nasdaqcom_macd_diff',
                     'nasdaqcom_trix', 'nasdaqcom_sma_26', 'nasdaqcom_sma_12', 'nasdaqcom_sma_9', 'nasdaqcom_log', 't10y3m_aroon_indicator', 't10y3m_ema_indicator', 't10y3m_macd_diff', 't10y3m_trix', 't10y3m_sma_26', 't10y3m_sma_12', 't10y3m_sma_9', 't10y3m_diff', 'month_aroon_indicator', 'month_ema_indicator', 'month_macd_diff', 'month_trix', 'month_sma_26', 'month_sma_12', 'month_sma_9', 'month_diff', 'weekday_aroon_indicator', 'weekday_ema_indicator', 'weekday_macd_diff', 'weekday_trix', 'weekday_sma_26', 'weekday_sma_12', 'weekday_sma_9', 'weekday_diff', 'quarter_aroon_indicator', 'quarter_ema_indicator', 'quarter_macd_diff', 'quarter_trix', 'quarter_sma_26', 'quarter_sma_12', 'quarter_sma_9', 'quarter_diff', 'week_aroon_indicator', 'week_ema_indicator', 'week_macd_diff', 'week_trix', 'week_sma_26', 'week_sma_12', 'week_sma_9', 'week_diff']
 
-    print('Current working directory is: ',os.getcwd())
+    trained_columns_regressor = ['nasdaqcom_trix', 'nasdaqcom_macd_diff', 'vixcls', 'vixcls_macd_diff', 'vixcls_diff', 'dff_macd_diff', 'bamlh0a0hym2_macd_diff', 'dcoilwtico_diff']
+
     try:
-        model = load(os.path.join('data', 'random_forest_classifier.joblib'))
+        classifier_model = load(os.path.join('data', 'random_forest_classifier.joblib'))
     except:
-        model = load(os.path.join('machine_learning', 'data', 'random_forest_classifier.joblib'))
+        classifier_model = load(os.path.join('machine_learning', 'data', 'random_forest_classifier.joblib'))
+
+    try:
+        regressor_model = load(os.path.join('data', 'random_forest_regressor.joblib'))
+    except:
+        regressor_model = load(os.path.join('machine_learning', 'data', 'random_forest_regressor.joblib'))
 
     fred_series_to_capture = ['DFF', 'BAMLH0A0HYM2',
                             'DCOILWTICO', 'VIXCLS', 'T10Y3M', 'NASDAQCOM']
@@ -94,31 +97,39 @@ def solicit_inferences(fred_api_key, aws_access_key_id, aws_secret_access_key, b
 
     dataset.drop(columns=['nasdaqcom'], inplace=True)
 
-    dataset.head()
-    dataset = dataset[trained_columns]
-    most_recent_records = dataset.tail(1)
+    def narrow_dataset_to_most_recent_records_and_required_columns(dataset,trained_columns):
+        dataset = dataset[trained_columns].copy()
+        most_recent_records = dataset.tail(1)
+        return most_recent_records
 
-    prediction = model.predict(most_recent_records)
+    most_recent_records_classifier = narrow_dataset_to_most_recent_records_and_required_columns(dataset,trained_columns_classifier)
+    most_recent_records_regressor = narrow_dataset_to_most_recent_records_and_required_columns(dataset,trained_columns_regressor)
 
-    print('Checking for previous predictions in S3 bucket...')
-    prediction_df = check_s3_bucket_for_previous_predictions(
-        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, bucket_name=bucket_name)
+    def make_predictions(most_recent_records,trained_model,file_name):
+        prediction = trained_model.predict(most_recent_records)
 
-    prediction_df['date'] = pd.to_datetime(prediction_df['date'])
-    most_recent_records.index = pd.to_datetime(most_recent_records.index)
+        print('Checking for previous predictions in S3 bucket...')
+        prediction_df = check_s3_bucket_for_previous_predictions(
+            aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, bucket_name=bucket_name, file_name=file_name)
 
-    if most_recent_records.index[0] in prediction_df['date'].values:
-        print('Most recent prediction already exists in S3 bucket.')
-        pass
-    else:
-        print('Most recent prediction does not exist in S3 bucket. Adding...')
-        prediction_df = pd.concat([prediction_df, pd.DataFrame({'date': most_recent_records.index[0], 'prediction': prediction[0]},index=[0])], axis=0, ignore_index=True)
+        prediction_df['date'] = pd.to_datetime(prediction_df['date'])
+        most_recent_records.index = pd.to_datetime(most_recent_records.index)
 
-    print('Saving predictions to S3 bucket...')
-    upload_inferences_to_s3(
-        inferences=prediction_df, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, bucket_name=bucket_name)
+        if most_recent_records.index[0] in prediction_df['date'].values:
+            print('Most recent prediction already exists in S3 bucket.')
+            pass
+        else:
+            print('Most recent prediction does not exist in S3 bucket. Adding...')
+            prediction_df = pd.concat([prediction_df, pd.DataFrame({'date': most_recent_records.index[0], 'prediction': prediction[0]},index=[0])], axis=0, ignore_index=True)
 
-    print(prediction_df)
+        print('Saving predictions to S3 bucket...')
+        upload_inferences_to_s3(
+            inferences=prediction_df, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, bucket_name=bucket_name, file_name=file_name)
+
+        print(prediction_df)
+
+    make_predictions(most_recent_records_classifier,classifier_model,'classifier_inferences')
+    make_predictions(most_recent_records_regressor,regressor_model,'regressor_inferences')
 
     return
 
