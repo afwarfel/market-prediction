e4e6c2af0447274d2e3eea3560433b979ee99089
alexwarfel@gmail.com <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>
2022-12-04 12:26:17 -0800

    Updated line chart formatting

--- react/price-prediction/src/App.js
+++ react/price-prediction/src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import LineChartW from './LineChartW'
-import data from './data/inferences.json'
+import PredictionsLineChart from './PredictionsLineChart'
+import regressor_inferences from './data/regressor_inferences.json'
 
-function App() {
+const App = () => {
 
-  console.log(data)
+  const data = regressor_inferences
 
   return (
     <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          See daily predictions below:
-        </h1>
-      <LineChartW/>
+          <h1>
+            See daily predictions below:
+          </h1>
+        <PredictionsLineChart data={data}/>
+        <hr/>
+        <a style={{color:'lightblue'}} href='https://github.com/afwarfel/market-prediction/blob/main/machine_learning/random_forest_regressor.ipynb'>Review details of the model here!</a>
       </header>
     </div>
     </>
---
+++ react/price-prediction/src/PredictionsLineChart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+export const PredictionsLineChart = (data) => {
+
+  data = data.data.map((d) => {
+    return {
+      date: d.date,
+      prediction: (Math.round((d.prediction*100)*100)/100),
+      nasdaq_percent_change: (Math.round((d.nasdaq_percent_change*100)*100)/100)
+    }})
+
+    console.log('data',data)
+
+    return (
+      <ResponsiveContainer width={800} height={800}>
+        <LineChart
+          width={1500}
+          height={1300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" dy={20}/>
+          <YAxis tickFormatter={(tick) => {
+            return `${tick}%`;
+          }} dx={-10}/>
+          <Tooltip formatter={(value) => `${value}%`}/>
+          <Legend wrapperStyle={{
+        paddingTop: "40px"
+    }}/>
+          <Line name='Predicted NASDAQ Return' type="monotone" dataKey="prediction" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line name='Actual NASDAQ Return' type="monotone" dataKey="nasdaq_percent_change" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    );};
+
+export default PredictionsLineChart;
