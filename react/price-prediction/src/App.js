0a6dc63cebafd7821f5bfedfa1d0c0b98c2cb48b
alexwarfel@gmail.com <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>
2022-12-15 22:02:41 -0500

    Update for website

--- react/price-prediction/src/App.js
+++ react/price-prediction/src/App.js
@@ -1,51 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import PredictionsLineChart from './PredictionsLineChart'
+import React, { useState, useEffect } from "react";
+// import logo from "./logo.svg";
+import logo from './logo.png'
+import "./App.css";
+import PredictionsLineChart from "./PredictionsLineChart";
 
 const App = () => {
   const [regressor_inferences, set_regressor_inferences] = useState([]);
   const [minute_level_inferences, set_minute_level_inferences] = useState([]);
-  // fetch('https://stock-market-prediction-front-end.s3.us-west-2.amazonaws.com/data/regressor_inferences.json', { mode: 'no-cors' })
-  //   .then(response => console.log(response))
 
-    function getJson(url) {
-      return fetch(url)
-        .then(response => response.json())
-        .catch(error => {
-          console.error(error);
-        });
-    }
+  function getJson(url) {
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-    useEffect(() => {
-      getJson('https://stock-market-prediction-front-end.s3.us-west-2.amazonaws.com/data/regressor_inferences.json').then(regressor_inferences => set_regressor_inferences(regressor_inferences));
-      getJson('https://stock-market-prediction-front-end.s3.us-west-2.amazonaws.com/data/regressor_inferences_minute.json').then(minute_level_inferences => set_minute_level_inferences(minute_level_inferences));
-    }, []);
+  useEffect(() => {
+    getJson(
+      "https://stock-market-prediction-front-end.s3.us-west-2.amazonaws.com/data/regressor_inferences.json"
+    ).then((regressor_inferences) =>
+      set_regressor_inferences(regressor_inferences)
+    );
+    getJson(
+      "https://stock-market-prediction-front-end.s3.us-west-2.amazonaws.com/data/regressor_inferences_minute.json"
+    ).then((minute_level_inferences) =>
+      set_minute_level_inferences(minute_level_inferences)
+    );
+  }, []);
 
-  console.log(minute_level_inferences)
-
-  const data = regressor_inferences
-  const minute_level_data = minute_level_inferences
+  const data = regressor_inferences;
+  const minute_level_data = minute_level_inferences;
 
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <h1>
-            See daily predictions below:
-          </h1>
-        <h2>Daily Level NASDAQ Returns</h2>
-        <PredictionsLineChart data={data}/>
-        <hr/>
-        <h2>Minute Level VTI Predictions</h2>
-        <PredictionsLineChart data={minute_level_data}/>
-        <br/>
-        <a style={{color:'lightblue',padding:'50px'}} href='https://github.com/afwarfel/market-prediction/blob/main/machine_learning/random_forest_regressor.ipynb'>Review details of the model here!</a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Daily Market Predictions</h1>
+          <h2>Daily Level NASDAQ Returns</h2>
+          <PredictionsLineChart data={data} />
+          <br style={{ width: "100px" }} />
+          <h2>15 Minute Level VTI Predictions</h2>
+          <PredictionsLineChart data={minute_level_data} />
+          <br />
+          <a
+            style={{ color: "#008DD5", padding: "50px" }}
+            href="https://github.com/afwarfel/market-prediction/blob/main/machine_learning/random_forest_regressor.ipynb"
+          >
+            Review details of the model here!
+          </a>
+        </header>
+      </div>
     </>
   );
-}
+};
 
 export default App;
--- react/price-prediction/src/PredictionsLineChart.js
+++ react/price-prediction/src/PredictionsLineChart.js
@@ -1,39 +1,71 @@
-import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 export const PredictionsLineChart = (data) => {
-
   data = data.data.map((d) => {
     return {
       date: d.date,
-      prediction: d.prediction===null?null:(Math.round((d.prediction*100)*100)/100),
-      actual_percent_change: d.actual_percent_change===null?null:(Math.round((d.actual_percent_change*100)*100)/100)
-    }})
+      prediction:
+        d.prediction === null
+          ? null
+          : Math.round(d.prediction * 100 * 100) / 100,
+      actual_percent_change:
+        d.actual_percent_change === null
+          ? null
+          : Math.round(d.actual_percent_change * 100 * 100) / 100,
+    };
+  });
 
-    return (
-      <ResponsiveContainer width={'95%'} height={800}>
-        <LineChart
-          data={data}
-          margin={{
-            top: 50,
-            right: 55,
-            left: 55,
-            bottom: 50,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" dy={20}/>
-          <YAxis tickFormatter={(tick) => {
+  return (
+    <ResponsiveContainer width={"95%"} height={800}>
+      <LineChart
+        data={data}
+        margin={{
+          top: 50,
+          right: 55,
+          left: 55,
+          bottom: 50,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" dy={20} />
+        <YAxis
+          tickFormatter={(tick) => {
             return `${tick}%`;
-          }} dx={-10}/>
-          <Tooltip formatter={(value) => `${value}%`}/>
-          <Legend wrapperStyle={{
-        paddingTop: "40px"
-    }}/>
-          <Line name='Predicted Return' type="monotone" dataKey="prediction" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line name='Actual Observed Return' type="monotone" dataKey="actual_percent_change" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-    );};
+          }}
+          dx={-10}
+        />
+        <Tooltip formatter={(value) => `${value}%`} />
+        <Legend
+          wrapperStyle={{
+            paddingTop: "40px",
+          }}
+        />
+        <Line
+          name="Predicted Return"
+          type="monotone"
+          dataKey="prediction"
+          stroke="#C20114"
+          activeDot={{ r: 8 }}
+        />
+        <Line
+          name="Actual Observed Return"
+          type="monotone"
+          dataKey="actual_percent_change"
+          stroke="#008DD5"
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default PredictionsLineChart;
